bbf727ff41d37b9e927f81e3b109d7f5f9e7fdda
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-18 21:51:36 +0400

    Update Num1.java

--- Num1.java
+++ Num1.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 public class Num1 {
-    public static void main(String[] arg) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int a = sc.nextInt();
-        
-        
-        if (a > 0) {
-            System.out.println("Positive number");
-        } else if (a < 0) {
-            System.out.println("Negative number");
-        } else {
-            System.out.println("The number equals zero");
-        }
-       
+  public static void main(String[] arg) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number: ");
+    int a = sc.nextInt();
+
+    if (a > 0) {
+      System.out.println("Positive number");
+    } else if (a < 0) {
+      System.out.println("Negative number");
+    } else {
+      System.out.println("The number equals zero");
     }
+
+  }
 }
